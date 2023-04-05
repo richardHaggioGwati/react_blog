d8f77f732aafce4b60d04ff5331f3384de5005d1
Richard Gwati <haggio0844@gmail.com>
2023-04-05 19:22:27 +0200

    feat(link preview): implementing link preview

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -4,8 +4,11 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <head>
-        <title>Template</title>
-        <meta name="description" content="App Template" />
+        <title>Crafters</title>
+        <meta
+          name="description"
+          content="My Next.js blog is a platform for sharing my thoughts and experiences with any and all things code related.It is currently running on Next.js which is a popular React-based framework for building server-side rendered (SSR) web applications that provide better performance, SEO, and user experience than traditional client-side rendered apps."
+        />
         <link rel="icon" href="/favicon.ico" />
       </head>
 
--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -5,8 +5,21 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
     <html lang="en">
       <head>
         <title>Crafters</title>
-        <meta name="description" content="daily blog" />
+        <meta
+          name="description"
+          content="My Next.js blog is a platform for sharing my thoughts and experiences with any and all things code related.It is currently running on Next.js which is a popular React-based framework for building server-side rendered (SSR) web applications that provide better performance, SEO, and user experience than traditional client-side rendered apps."
+        />
         <link rel="icon" href="/king.svg" />
+        <meta property="og:title" content="Code Crafters" />
+        <meta property="og:type" content="blog" />
+        <meta
+          property="og:image"
+          content="https://images.unsplash.com/photo-1652841199247-f9ccbcbe4682?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
+        />
+        <meta
+          property="og:description"
+          content="My Next.js blog is a platform for sharing my thoughts and experiences with any and all things code related"
+        />
       </head>
 
       <body className="bg-[#2b2b2b]">
