302e7e81ca207eb4ad9ff8179b228381b75097a8
Richard Gwati <haggio0844@gmail.com>
2023-04-11 17:55:57 +0200

    feat: bird background

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,6 +1,24 @@
+'use client';
+
+import { useEffect } from 'react';
 import '../../css/globals.css';
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  useEffect(() => {
+    const threeScript = document.createElement('script');
+    threeScript.setAttribute('id', 'threeScript');
+    threeScript.setAttribute(
+      'src',
+      'https://cdnjs.cloudflare.com/ajax/libs/three.js/r124/three.min.js',
+    );
+    document.getElementsByTagName('head')[0].appendChild(threeScript);
+    return () => {
+      if (threeScript) {
+        threeScript.remove();
+      }
+    };
+  }, []);
+
   return (
     <html lang="en">
       <head>
@@ -14,7 +32,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
         <meta property="og:type" content="blog" />
         <meta
           property="og:image"
-          content="https://images.unsplash.com/photo-1652841199247-f9ccbcbe4682?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
+          content="https://images.unsplash.com/photo-1452421822248-d4c2b47f0c81?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
         />
         <meta
           property="og:description"
@@ -23,7 +41,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
       </head>
 
       <body className="bg-[#2b2b2b]">
-        <div className="max-w-7xl mx-auto">{children}</div>
+        <div>{children}</div>
       </body>
     </html>
   );
